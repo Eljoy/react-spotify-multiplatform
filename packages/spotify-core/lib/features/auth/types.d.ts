5018f8e039ddf6c565fcee6a7d42350bde4bb634
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-19 11:35:59 +0500

    Created auth namespace

---
+++ packages/spotify-core/lib/features/auth/types.d.ts
@@ -0,0 +1,6 @@
+import type { Scopes, ResponseType, SpotifyAuthUrlProvider } from './data'
+import type { OauthService } from './domain'
+
+declare namespace Auth {
+  export { Scopes, ResponseType, SpotifyAuthUrlProvider, OauthService }
+}
