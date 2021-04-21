012d4cce6f8ec8cb870578a37f66c9b18cc4da36
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-22 00:05:48 +0500

    Modified toString and removed deserialize static method

--- packages/spotify-core/lib/entities/Entity.ts
+++ packages/spotify-core/lib/entities/Entity.ts
@@ -1,16 +1,12 @@
-import { Serializable, deserialize, serialize } from "typescript-json-serializer";
+import { Serializable, serialize } from "typescript-json-serializer";
 
 @Serializable()
 export default class Entity {
-  static deserialize<T extends Entity>(entityDao: Record<string, unknown>): T {
-    return deserialize(entityDao, this) as T;
-  }
-
   serialize(): Record<string, unknown> {
     return serialize(this);
   }
 
   toString(): string {
-    return JSON.stringify(this);
+    return JSON.stringify(this.serialize());
   }
 }
