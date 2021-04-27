9750b7e46607f331513c0a41f999b364d22b25f7
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-27 17:29:03 +0500

    Added signOut.request support

--- packages/spotify-core/lib/features/auth/adapters/authSaga.ts
+++ packages/spotify-core/lib/features/auth/adapters/authSaga.ts
@@ -1,38 +1,49 @@
-import { AppDependencies } from '../../../dependencies'
-import { put, call, takeLatest, take, all, fork } from 'redux-saga/effects'
-import { signIn } from '../data'
-import { buffers, eventChannel } from 'redux-saga'
-import { Token } from '../entities'
-import { AuthRepository } from '../domain'
-import { byLazy } from '../../../common'
-import { spotifyAppContainer } from '../../../inversify.config'
+import { AppDependencies } from "../../../dependencies";
+import { put, call, takeLatest, take, all, fork } from "redux-saga/effects";
+import { signIn, signOut } from "../data";
+import { buffers, eventChannel } from "redux-saga";
+import { Token } from "../entities";
+import { AuthRepository } from "../domain";
+import { byLazy } from "../../../common";
+import { spotifyAppContainer } from "../../../inversify.config";
 
-const authRepository = byLazy(() => spotifyAppContainer.get<AuthRepository>(AppDependencies.SPOTIFY_AUTH_REPOSITORY))
+const authRepository = byLazy(() => spotifyAppContainer.get<AuthRepository>(AppDependencies.SPOTIFY_AUTH_REPOSITORY));
 
 export default function* authSaga() {
-  const token = yield call(authRepository().getAuthToken)
+  const token = yield call(authRepository().getAuthToken);
   if (token) {
-    yield put(signIn.success(token))
+    yield put(signIn.success(token));
   }
-  yield all([takeLatest(signIn.request, promptOauthSignInFlow), fork(watchAuthChange)])
+  yield all([
+    takeLatest(signIn.request, promptOauthSignInFlow),
+    takeLatest(signOut.request, signOutSaga),
+    fork(watchAuthChange)
+  ]);
 }
 
 function* promptOauthSignInFlow() {
-  yield call(authRepository().promptOauthSignInFlow)
+  yield call(authRepository().promptOauthSignInFlow);
+}
+
+function* signOutSaga() {
+  yield call([authRepository(), "signOut"]);
+  yield put(signOut.success());
 }
 
 function* watchAuthChange() {
   const channel = eventChannel(emitter => {
     const subscriber = (token: Token) => {
-      emitter(token)
-    }
-    authRepository().subscribe(subscriber)
+      emitter({ token });
+    };
+    authRepository().subscribe(subscriber);
     return () => {
-      authRepository().unsubscribe(subscriber)
-    }
-  }, buffers.sliding(1))
+      authRepository().unsubscribe(subscriber);
+    };
+  }, buffers.sliding(1));
   while (true) {
-    const token: Token = yield take(channel)
-    yield put(signIn.success(token))
+    const { token } = yield take(channel);
+    if (token) {
+      yield put(signIn.success(token));
+    }
   }
 }
