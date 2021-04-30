34701f801dff3e2b04c59c2215fc45c0a3593230
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-30 13:47:35 +0500

    Added getAllKeys and multiRemove methods added localForage support

--- packages/spotify-web/src/app/dependencies/WebCacheService.ts
+++ packages/spotify-web/src/app/dependencies/WebCacheService.ts
@@ -1,22 +1,35 @@
-import { CacheService } from "spotify-core";
-import { injectable } from "inversify";
+import { injectable } from 'inversify'
+import localforage from 'localforage'
+import { CacheService } from 'spotify-core'
 
 @injectable()
 export default class WebCacheService extends CacheService {
+  private localforageInstance = localforage.createInstance({})
+
   async removeItem(key: string): Promise<void> {
-    return window.localStorage.removeItem(key);
+    return this.localforageInstance.removeItem(key)
   }
 
   async get<T>(key: string): Promise<T | null> {
-    const itemStr = window.localStorage.getItem(key);
+    const itemStr = await this.localforageInstance.getItem<string>(key)
     try {
-      return itemStr ? JSON.parse(itemStr) as unknown as T : null;
+      return itemStr ? ((JSON.parse(itemStr) as unknown) as T) : null
     } catch (e) {
-      return itemStr as unknown as T | null;
+      return (itemStr as unknown) as T | null
     }
   }
 
   async put(key: string, item: string): Promise<void> {
-    return window.localStorage.setItem(key, item);
+    await this.localforageInstance.setItem(key, item)
+  }
+
+  getAllKeys(): Promise<string[]> {
+    return this.localforageInstance.keys()
+  }
+
+  async multiRemove(keys: string[]): Promise<void> {
+    await Promise.all(
+      keys.map((key) => this.localforageInstance.removeItem(key))
+    )
   }
 }
