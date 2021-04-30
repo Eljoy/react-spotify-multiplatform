e60afd16c0f5133ab9fa24401865882d24ef4863
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-30 13:46:57 +0500

    Added getAllKeys and multiRemove methods

--- packages/spotify-native/app/dependencies/NativeCacheService.ts
+++ packages/spotify-native/app/dependencies/NativeCacheService.ts
@@ -1,23 +1,31 @@
-import { CacheService } from "spotify-core";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { injectable } from "inversify";
+import {injectable} from 'inversify';
+import {CacheService} from 'spotify-core';
 
 @injectable()
 export default class NativeCacheService extends CacheService {
   async get<T>(key: string): Promise<T | null> {
     const itemStr = await AsyncStorage.getItem(key);
     try {
-      return itemStr ? JSON.parse(itemStr) as unknown as T : null;
+      return itemStr ? ((JSON.parse(itemStr) as unknown) as T) : null;
     } catch (e) {
-      return itemStr as unknown as T | null;
+      return (itemStr as unknown) as T | null;
     }
   }
 
   put(key: string, item: string): Promise<void> {
-    return AsyncStorage.setItem(key, item)
+    return AsyncStorage.setItem(key, item);
   }
 
   removeItem(key: string): Promise<void> {
-    return AsyncStorage.removeItem(key)
+    return AsyncStorage.removeItem(key);
+  }
+
+  getAllKeys(): Promise<string[]> {
+    return AsyncStorage.getAllKeys();
+  }
+
+  multiRemove(keys: string[]): Promise<void> {
+    return AsyncStorage.multiRemove(keys);
   }
 }
