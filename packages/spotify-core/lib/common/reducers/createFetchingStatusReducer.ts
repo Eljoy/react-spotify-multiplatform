2687dd3782cc5aced6968ef9da1e36b200889617
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-04-30 16:38:15 +0500

    Renamed NotFetched -> Idle

--- packages/spotify-core/lib/common/reducers/createFetchingStatusReducer.ts
+++ packages/spotify-core/lib/common/reducers/createFetchingStatusReducer.ts
@@ -1,19 +1,27 @@
-import { Action, Reducer, AsyncActionCreatorBuilder } from "typesafe-actions";
-import { RootStateOrAny } from "react-redux";
-import { createReducer } from "typesafe-actions";
+import { RootStateOrAny } from 'react-redux'
+import {
+  Action,
+  AsyncActionCreatorBuilder,
+  createReducer,
+  Reducer,
+} from 'typesafe-actions'
 
 export enum FetchingStatus {
-  NotFetched = 'NotFetched',
+  Idle = 'Idle',
   Fetching = 'Fetching',
   Fetched = 'Fetched',
-  FetchingError = 'FetchingError'
+  FetchingError = 'FetchingError',
 }
 
-export default function createFetchingStatusReducer<State,
-  TAction extends Action>(asyncAction: AsyncActionCreatorBuilder<any, any, any>,
-                          initialState = FetchingStatus.NotFetched): Reducer<FetchingStatus, RootStateOrAny> {
+export default function createFetchingStatusReducer<
+  State,
+  TAction extends Action
+>(
+  asyncAction: AsyncActionCreatorBuilder<any, any, any>,
+  initialState = FetchingStatus.Idle
+): Reducer<FetchingStatus, RootStateOrAny> {
   return createReducer(initialState)
     .handleAction(asyncAction.request, () => FetchingStatus.Fetching)
     .handleAction(asyncAction.success, () => FetchingStatus.Fetched)
-    .handleAction(asyncAction.failure, () => FetchingStatus.FetchingError);
+    .handleAction(asyncAction.failure, () => FetchingStatus.FetchingError)
 }
