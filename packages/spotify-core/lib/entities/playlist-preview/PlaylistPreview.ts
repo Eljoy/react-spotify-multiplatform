d26be387a437c6a690ab2bdcdf694f81f334d2a0
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-20 21:53:24 +0500

    Added playlistPreview entity

---
+++ packages/spotify-core/lib/entities/playlist-preview/PlaylistPreview.ts
@@ -0,0 +1,25 @@
+import { JsonProperty, SerializableEntity } from 'ts-jackson'
+import { Image } from '../image'
+
+export default class PlaylistPreview extends SerializableEntity {
+  @JsonProperty({ required: true })
+  readonly id: string
+
+  @JsonProperty()
+  readonly name: string
+
+  @JsonProperty()
+  readonly description: string
+
+  @JsonProperty('images[0]')
+  readonly backgroundImage: Image
+
+  @JsonProperty()
+  readonly href: string
+
+  @JsonProperty({ required: true })
+  readonly tracks: {
+    href: string
+    total: number
+  }
+}
