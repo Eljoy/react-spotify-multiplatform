5bf24cac2d81eb42f8e8c2e4e7e0e50aa279f132
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-20 21:43:30 +0500

    CacheService abstract class -> interface

--- packages/spotify-core/lib/common/cache/CacheService.ts
+++ packages/spotify-core/lib/common/cache/CacheService.ts
@@ -1,30 +1,11 @@
-import { Entities } from '../../entities'
+export interface CacheService {
+  put(key: string, item: string): Promise<void>
 
-enum Keys {
-  Token = 'Token',
-}
+  get<T>(key: string): Promise<T | null>
 
-export abstract class CacheService {
-  async getToken(): Promise<Entities.Token | null> {
-    const tokenDAO: Record<string, unknown> = await this.get(Keys.Token)
-    return tokenDAO ? Entities.Token.deserialize(tokenDAO) : null
-  }
+  removeItem(key: string): Promise<void>
 
-  async putToken(token: Entities.Token): Promise<void> {
-    return this.put(Keys.Token, token.toString())
-  }
+  multiRemove(keys: string[]): Promise<void>
 
-  async removeToken(): Promise<void> {
-    return this.removeItem(Keys.Token)
-  }
-
-  abstract put(key: string, item: string): Promise<void>
-
-  abstract get<T>(key: string): Promise<T | null>
-
-  abstract removeItem(key: string): Promise<void>
-
-  abstract multiRemove(keys: string[]): Promise<void>
-
-  abstract getAllKeys(): Promise<string[]>
+  getAllKeys(): Promise<string[]>
 }
