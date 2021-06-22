7a30984ea0bb6ade21b2e2e048e1015a5420fce9
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-22 13:24:15 +0500

    Added  Auth.SpotifyAuthUrlProvider injection

--- packages/spotify-native/inversify.config.ts
+++ packages/spotify-native/inversify.config.ts
@@ -3,18 +3,26 @@ import {
   AppDependencies,
   Auth,
   CacheService,
+  LinkingService,
   spotifyAppContainer,
 } from 'spotify-core';
-import {NativeCacheService} from './app/dependencies';
-import NativeSpotifyAuthService from './app/dependencies/NativeSpotifyAuthService';
-
-spotifyAppContainer
-  .bind<Auth.SpotifyAuthService>(AppDependencies.Auth.Service)
-  .to(NativeSpotifyAuthService)
-  .inSingletonScope();
+import {NativeCacheService, NativeLinkingService} from './app/dependencies';
 
 spotifyAppContainer
   .bind<CacheService>(AppDependencies.Common.CacheService)
   .to(NativeCacheService);
+
+spotifyAppContainer
+  .bind<LinkingService>(AppDependencies.Common.LinkingService)
+  .to(NativeLinkingService);
+
+const spotifyAuthUrlProvider = Auth.SpotifyAuthUrlProvider.create()
+  .addScopes([Auth.Scopes.userLibraryRead, Auth.Scopes.streaming])
+  .setClientId('06006394f03e41b9af557e5e00ab2220')
+  .setRedirectUri('reactspotifymultiplatform://page.link');
+
+spotifyAppContainer
+  .bind<Auth.SpotifyAuthUrlProvider>(AppDependencies.Auth.UrlProvider)
+  .toConstantValue(spotifyAuthUrlProvider);
 
 spotifyAppContainer.load(buildProviderModule());
