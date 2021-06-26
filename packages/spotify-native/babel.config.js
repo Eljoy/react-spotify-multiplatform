eedd85daa5eadfd6a0628704c30a58bbb2cc8788
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-26 13:34:18 +0500

    Added "module:react-native-dotenv" plugin

--- packages/spotify-native/babel.config.js
+++ packages/spotify-native/babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["module:react-native-dotenv"],
     ["@babel/plugin-transform-runtime", {
       "helpers": true,
       "regenerator": false
