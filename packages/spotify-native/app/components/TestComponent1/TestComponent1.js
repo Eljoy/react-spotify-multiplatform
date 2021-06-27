f3e2a27232ee9c1bc7a1cbe36a9e972a502269a0
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-27 22:31:20 +0500

    Storybook react native basic setup

---
+++ packages/spotify-native/app/components/TestComponent1/TestComponent1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function TestComponent1() {
+  return (
+    <View>
+      <Text>TestComponent1</Text>
+    </View>
+  )
+}
+
+export default TestComponent1
