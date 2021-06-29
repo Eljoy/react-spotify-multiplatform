fa064ab274a042d3b5ae36b02b5e6c0c5d6da578
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-06-29 12:48:10 +0500

    By lazy minor styling changes

--- packages/spotify-core/lib/common/by-lazy/byLazy.ts
+++ packages/spotify-core/lib/common/by-lazy/byLazy.ts
@@ -1,9 +1,9 @@
 export default function byLazy<ArgsType, ResultType>(
   fn: (...args: ArgsType[]) => ResultType
-) {
+): typeof fn {
   let result: ResultType
   return (...args: ArgsType[]) => {
-    result = result || fn(...args)
+    result ||= fn(...args)
     return result
   }
 }
