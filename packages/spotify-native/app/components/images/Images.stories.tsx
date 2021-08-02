24d995ea16801168125d8cfcee34b68aab93e54e
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-08-02 12:39:01 +0500

    Removed ImageShadow

--- packages/spotify-native/app/components/images/Images.stories.tsx
+++ packages/spotify-native/app/components/images/Images.stories.tsx
@@ -2,23 +2,30 @@ import {storiesOf} from '@storybook/react-native';
 import React from 'react';
 import {useAppTheme} from '../../theme';
 import Layout from '../layout/Layout';
+import BodyText from '../typography/BodyText';
 import {Cover} from './Cover';
 import {Thumbnail} from './Thumbnail';
 
 storiesOf('Images', module).add('Images', () => <ImagesStory />);
 
 const uri = 'https://m.media-amazon.com/images/I/61d8t0gNa+L._SS500_.jpg';
+const source = {uri};
 
 function ImagesStory() {
   const {colors} = useAppTheme();
-
   return (
     <Layout
       flex={1}
       style={{backgroundColor: colors.background}}
-      align="space-evenly center">
-      <Cover source={{uri}} />
-      <Thumbnail source={{uri}} />
+      align="start start">
+      <Layout style={{margin: 10}}>
+        <BodyText>Cover</BodyText>
+        <Cover source={source} />
+      </Layout>
+      <Layout style={{margin: 10}}>
+        <BodyText>Thumbnail</BodyText>
+        <Thumbnail source={source} />
+      </Layout>
     </Layout>
   );
 }
