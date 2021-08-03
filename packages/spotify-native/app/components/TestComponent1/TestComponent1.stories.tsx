561520019ec2510170142c035599941c02eed4ad
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-08-03 19:10:28 +0500

    Removed TestComponent1.tsx

--- packages/spotify-native/app/components/TestComponent1/TestComponent1.stories.tsx
+++
@@ -1,5 +0,0 @@
-import {storiesOf} from '@storybook/react-native';
-import React from 'react';
-import TestComponent1 from './TestComponent1';
-
-storiesOf('Test', module).add('example', () => <TestComponent1 />);
