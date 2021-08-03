4eaf1981526c21fd74e0a8e5491dd09514045fe0
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-08-03 19:09:15 +0500

    Added typography indexing

--- packages/spotify-native/app/components/typography/index.ts
+++ packages/spotify-native/app/components/typography/index.ts
@@ -1,1 +1,6 @@
 export {default as AppText} from './AppText';
+export {default as BodyText} from './BodyText';
+export {default as ButtonText} from './ButtonText';
+export {default as CaptionText} from './CaptionText';
+export {default as SubTitleText} from './SubTitleText';
+export {default as TitleText} from './TitleText';
