407f8ec44057fa301dc2ff0557ffdacc413f5c2e
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-13 13:25:36 +0500

    Added stories

--- packages/spotify-native/storybook/storyLoader.js
+++ packages/spotify-native/storybook/storyLoader.js
@@ -4,10 +4,24 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../app/components/TestComponent1/TestComponent1.stories');
+  require('../app/components/buttons/Buttons.stories');
+  require('../app/components/containers/Containers.stories');
+  require('../app/components/features/featured-playlists/PlaylistPreview.stories');
+  require('../app/components/gradient/GradientShadow.stories');
+  require('../app/components/images/Images.stories');
+  require('../app/components/layout/Layout.stories');
+  require('../app/components/typography/Typography.stories');
 }
 
-const stories = ['../app/components/TestComponent1/TestComponent1.stories'];
+const stories = [
+  '../app/components/buttons/Buttons.stories',
+  '../app/components/containers/Containers.stories',
+  '../app/components/features/featured-playlists/PlaylistPreview.stories',
+  '../app/components/gradient/GradientShadow.stories',
+  '../app/components/images/Images.stories',
+  '../app/components/layout/Layout.stories',
+  '../app/components/typography/Typography.stories',
+];
 
 module.exports = {
   loadStories,
