60eab2f4796f9ce0c023e99b58201e0bb5d0125c
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-13 13:17:30 +0500

    Created Containers.stories.tsx

---
+++ packages/spotify-native/app/components/containers/Containers.stories.tsx
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/react-native'
+import React from 'react'
+import { BlurredBackground } from './BlurredBackground'
+import { Image } from 'react-native'
+import Layout from '../layout/Layout'
+import { AppBackground } from './AppBackground'
+
+storiesOf('Containers', module)
+  .add('BlurredBackground', () => <BlurredBackgroundStory />)
+  .add('AppBackground', () => <AppBackgroundStory />)
+
+const uri = 'https://m.media-amazon.com/images/I/61d8t0gNa+L._SS500_.jpg'
+
+function BlurredBackgroundStory() {
+  return (
+    <AppBackground>
+      <Layout flex={1} align='center center'>
+        <BlurredBackground>
+          <Image source={{ uri }} style={{ width: 300, height: 300 }} />
+        </BlurredBackground>
+      </Layout>
+    </AppBackground>
+  )
+}
+
+function AppBackgroundStory() {
+  return <AppBackground/>
+}
