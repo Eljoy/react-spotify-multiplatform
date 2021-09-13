afed1f438159ebcd6e2289b60887b91ba3f0d7d3
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-13 13:24:16 +0500

    Replaced Layout with AppBackground

--- packages/spotify-native/app/components/typography/Typography.stories.tsx
+++ packages/spotify-native/app/components/typography/Typography.stories.tsx
@@ -1,12 +1,12 @@
 import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import {AppThemeProvider, useAppTheme} from '../../theme';
-import {Layout} from '../layout';
+import {AppThemeProvider} from '../../theme';
 import BodyText from './BodyText';
 import ButtonText from './ButtonText';
 import CaptionText from './CaptionText';
 import SubTitleText from './SubTitleText';
 import TitleText from './TitleText';
+import { AppBackground } from '../containers'
 
 storiesOf('Typography', module).add('AppText', () => {
   return (
@@ -17,17 +17,13 @@ storiesOf('Typography', module).add('AppText', () => {
 });
 
 function TypographyStory() {
-  const theme = useAppTheme();
   return (
-    <Layout
-      flex={1}
-      style={{backgroundColor: theme.colors.background}}
-      paddingScale={2}>
+    <AppBackground paddingScale={2}>
       <TitleText>TITLE TEXT</TitleText>
       <ButtonText>BUTTON TEXT</ButtonText>
       <SubTitleText>Subtitle Text</SubTitleText>
       <BodyText>Body Text</BodyText>
       <CaptionText>Caption Text</CaptionText>
-    </Layout>
+    </AppBackground>
   );
 }
