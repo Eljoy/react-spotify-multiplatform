13aa2b500ccb10e2a4926848ddfb4c42e71378bf
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-14 16:58:42 +0500

    Added text-transform uppercase

--- packages/spotify-native/app/components/typography/ButtonText.tsx
+++ packages/spotify-native/app/components/typography/ButtonText.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import {TextProps} from 'react-native';
+import { StyleProp, TextProps, TextStyle } from 'react-native'
 import {TokenTypography} from '../../design-tokens';
 import {useAppTheme} from '../../theme';
 import AppText, {AppTextProps} from './AppText';
 
 function ButtonText({
   children,
+  style,
   ...props
 }: Pick<AppTextProps, 'children'> & TextProps) {
   const {colors} = useAppTheme();
+  const styles: StyleProp<TextStyle> = [style, {textTransform: 'uppercase'}]
   return (
     <AppText
       {...props}
       fontColor={colors.text.button}
       fontSize={TokenTypography.FontSize.Button}
       fontFamily={TokenTypography.FontFamily.Button}
-      fontWeight={TokenTypography.FontWeight.Button}>
+      fontWeight={TokenTypography.FontWeight.Button}
+      style={styles}
+    >
       {children}
     </AppText>
   );
--- packages/spotify-native/app/components/typography/TitleText.tsx
+++ packages/spotify-native/app/components/typography/TitleText.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {TextProps} from 'react-native';
+import { StyleProp, TextProps, TextStyle } from 'react-native'
 import {TokenTypography} from '../../design-tokens';
 import {useAppTheme} from '../../theme';
 import AppText, {AppTextProps} from './AppText';
 
 function TitleText({
   children,
+  style,
   ...props
 }: Pick<AppTextProps, 'children'> & TextProps) {
+  const styles: StyleProp<TextStyle> = [style, {textTransform: 'uppercase'}]
   const {colors} = useAppTheme();
   return (
     <AppText
@@ -16,6 +18,7 @@ function TitleText({
       fontFamily={TokenTypography.FontFamily.Title}
       fontSize={TokenTypography.FontSize.Title}
       fontWeight={TokenTypography.FontWeight.Title}>
+      style={styles}
       {children}
     </AppText>
   );
