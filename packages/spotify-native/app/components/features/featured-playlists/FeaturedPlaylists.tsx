147c8c4e1e98463a78c93604d2572478b7ea9668
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-14 17:00:39 +0500

    Added header to FeaturedPlaylists.tsx

--- packages/spotify-native/app/components/features/featured-playlists/FeaturedPlaylists.tsx
+++ packages/spotify-native/app/components/features/featured-playlists/FeaturedPlaylists.tsx
@@ -1,36 +1,54 @@
-import React, {useEffect} from 'react';
-import {FlatList, Text, View} from 'react-native';
-import {FeaturedPlaylistsFeature} from 'spotify-core';
-import PlaylistPreview from './PlaylistPreview';
+import React, { useEffect } from 'react'
+import { FlatList, Text, View } from 'react-native'
+import { FeaturedPlaylistsFeature, generatePlaylistImage, UserFeature, Auth } from 'spotify-core'
+import { PlaylistPreview } from './PlaylistPreview'
+import Layout from '../../layout/Layout'
+import { SubTitleText, TitleText } from '../../typography'
+import { Avatar } from '../../images'
+import SecondaryButton from '../../buttons/SecondaryButton'
 
 type Props = {
   onPlaylistSelected: (playlistId: string) => void;
 };
 
-export default function FeaturedPlaylists({onPlaylistSelected}: Props) {
+export default function FeaturedPlaylists({ onPlaylistSelected }: Props) {
   const {
     featuredPlaylists,
     fetchFeaturedPlaylists,
-  } = FeaturedPlaylistsFeature.useFeaturedPlaylists();
+  } = FeaturedPlaylistsFeature.useFeaturedPlaylists()
   useEffect(() => {
-    fetchFeaturedPlaylists();
-  }, []);
+    fetchFeaturedPlaylists()
+  }, [])
+  const {currentUser} = UserFeature.useCurrentUser()
+  const {signOut} = Auth.useAuth()
   return (
-    <View>
-      <Text>FeaturedPlaylists</Text>
-      <FlatList
-        data={featuredPlaylists}
-        renderItem={({item}) => (
-          <PlaylistPreview
-            onPress={playlistPreview => {
-              onPlaylistSelected(playlistPreview.id);
-            }}
-            playlistPreview={item}
-          />
-        )}
-        alwaysBounceVertical
-        keyExtractor={playlist => playlist.id}
-      />
-    </View>
-  );
+    <Layout flex={1}>
+      <Layout height={60} direction='row' align='space-around center'>
+        <Layout width={100}>
+        <SubTitleText numberOfLines={2}>FEATURED PLAYLISTS</SubTitleText>
+        </Layout>
+        <SecondaryButton onPress={signOut}>
+          <Layout direction='row' align='center center'>
+            <Avatar source={{ uri: currentUser?.image.url }} />
+            <SecondaryButton.Text>Sign Out</SecondaryButton.Text>
+          </Layout>
+        </SecondaryButton>
+      </Layout>
+      <Layout flex={1}>
+        <FlatList
+          data={featuredPlaylists}
+          renderItem={({ item }) => (
+            <PlaylistPreview
+              onPress={playlistPreview => {
+                onPlaylistSelected(playlistPreview.id)
+              }}
+              playlistPreview={item}
+            />
+          )}
+          alwaysBounceVertical
+          keyExtractor={playlist => playlist.id}
+        />
+      </Layout>
+    </Layout>
+  )
 }
