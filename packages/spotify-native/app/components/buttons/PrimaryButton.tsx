3322b9d6df7229bdeaa7ea08424a46d726cb60d0
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-16 13:18:43 +0500

    Added style to Layout of PrimaryButton

--- packages/spotify-native/app/components/buttons/PrimaryButton.tsx
+++ packages/spotify-native/app/components/buttons/PrimaryButton.tsx
@@ -1,36 +1,38 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode } from 'react'
 import {
   TouchableNativeFeedback,
   TouchableNativeFeedbackProps,
   ViewStyle,
-} from 'react-native';
+} from 'react-native'
 import {
   ButtonTokens,
   getButtonHeightStyle,
   getButtonPaddingStyle,
   getButtonRoundnessStyle,
-} from '../../design-tokens';
-import {useAppTheme} from '../../theme';
-import Layout from '../layout/Layout';
-import ButtonText from '../typography/ButtonText';
+} from '../../design-tokens'
+import { useAppTheme } from '../../theme'
+import Layout from '../layout/Layout'
+import ButtonText from '../typography/ButtonText'
 
 type Props = {
   children?: ReactNode;
+  style?: ViewStyle
 } & TouchableNativeFeedbackProps;
 
-export default function PrimaryButton({children, ...props}: Props) {
-  const {colors} = useAppTheme();
+export default function PrimaryButton({ children, style, ...props }: Props) {
+  const { colors } = useAppTheme()
   const styles: ViewStyle[] = [
-    {backgroundColor: colors.buttons.primary},
+    { backgroundColor: colors.buttons.primary },
     getButtonRoundnessStyle(ButtonTokens.Roundness.PrimaryButton),
     getButtonPaddingStyle(ButtonTokens.Padding.PrimaryButton),
     getButtonHeightStyle(ButtonTokens.Height.PrimaryButton),
-  ];
+    style,
+  ]
   return (
     <TouchableNativeFeedback {...props}>
-      <Layout style={styles} align="center center">
+      <Layout style={styles} align='center center'>
         <ButtonText>{children}</ButtonText>
       </Layout>
     </TouchableNativeFeedback>
-  );
+  )
 }
