1325559b8135371b413ec047b3f4f04157512f85
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-16 13:05:35 +0500

    Put FeaturedPlaylistsHeader in FlatList

--- packages/spotify-native/app/components/features/featured-playlists/FeaturedPlaylists.tsx
+++ packages/spotify-native/app/components/features/featured-playlists/FeaturedPlaylists.tsx
@@ -19,36 +19,45 @@ export default function FeaturedPlaylists({ onPlaylistSelected }: Props) {
   useEffect(() => {
     fetchFeaturedPlaylists()
   }, [])
-  const {currentUser} = UserFeature.useCurrentUser()
-  const {signOut} = Auth.useAuth()
+  const data = ['header', ...featuredPlaylists]
   return (
     <Layout flex={1}>
-      <Layout height={60} direction='row' align='space-around center'>
-        <Layout width={100}>
-        <SubTitleText numberOfLines={2}>FEATURED PLAYLISTS</SubTitleText>
-        </Layout>
-        <SecondaryButton onPress={signOut}>
-          <Layout direction='row' align='center center'>
-            <Avatar source={{ uri: currentUser?.image.url }} />
-            <SecondaryButton.Text>Sign Out</SecondaryButton.Text>
-          </Layout>
-        </SecondaryButton>
-      </Layout>
-      <Layout flex={1}>
-        <FlatList
-          data={featuredPlaylists}
-          renderItem={({ item }) => (
+      <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          if(typeof item === 'string') {
+            return <FeaturedPlaylistsHeader/>
+          }
+          return (
             <PlaylistPreview
               onPress={playlistPreview => {
                 onPlaylistSelected(playlistPreview.id)
               }}
               playlistPreview={item}
             />
-          )}
-          alwaysBounceVertical
-          keyExtractor={playlist => playlist.id}
-        />
+          )
+        }}
+        alwaysBounceVertical
+        keyExtractor={item => typeof item === 'string' ? item: item.id}
+      />
+    </Layout>
+  )
+}
+
+function FeaturedPlaylistsHeader() {
+  const { currentUser } = UserFeature.useCurrentUser()
+  const { signOut } = Auth.useAuth()
+  return (
+    <Layout height={60} direction='row' align='space-around center'>
+      <Layout width={100}>
+        <SubTitleText numberOfLines={2}>FEATURED PLAYLISTS</SubTitleText>
       </Layout>
+      <SecondaryButton onPress={signOut}>
+        <Layout direction='row' align='center center'>
+          <Avatar source={{ uri: currentUser?.image.url }} />
+          <SecondaryButton.Text>Sign Out</SecondaryButton.Text>
+        </Layout>
+      </SecondaryButton>
     </Layout>
   )
 }
