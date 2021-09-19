22fd63c80a4cb02ef2e86e7bc8881ba8b8c92645
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-19 20:49:55 +0500

    Added Cover

--- packages/spotify-native/app/components/features/playlist/Playlist.tsx
+++ packages/spotify-native/app/components/features/playlist/Playlist.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {FlatList} from 'react-native';
 import {PlaylistFeature} from 'spotify-core';
+import {Cover} from '../../images';
 import Layout from '../../layout/Layout';
 import {TrackItem} from './TrackItem';
 
@@ -16,6 +17,7 @@ export default function Playlist({playlistId}: Props) {
   const tracks = playlist ? playlist.tracks : [];
   return (
     <Layout flex={1}>
+      <Cover source={{uri: playlist?.backgroundImage.url}} />
       <FlatList
         data={tracks}
         renderItem={({item}) => {
