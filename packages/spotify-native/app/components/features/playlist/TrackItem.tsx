d0931eabc3417c09de94ee6a7f370b96319a7303
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-19 20:51:45 +0500

    track.album.icon -> track.album.thumbnail

--- packages/spotify-native/app/components/features/playlist/TrackItem.tsx
+++ packages/spotify-native/app/components/features/playlist/TrackItem.tsx
@@ -1,71 +1,82 @@
-import React, { useMemo } from 'react'
-import { Entities } from 'spotify-core'
-import { getHeightStyle, getImageRoundnessStyle, IconTokens, ImageTokens, SizeTokens } from '../../../design-tokens'
-import { useAppTheme } from '../../../theme'
-import { BlurredBackground } from '../../containers/BlurredBackground'
-import { Icon } from '../../icons'
-import { Thumbnail } from '../../images'
-import { Layout } from '../../layout'
-import { BodyText, CaptionText } from '../../typography'
-import { TouchableNativeFeedback } from 'react-native'
-import noop from 'lodash.noop'
+import noop from 'lodash.noop';
+import React, {useMemo} from 'react';
+import {TouchableNativeFeedback} from 'react-native';
+import {Entities} from 'spotify-core';
+import {
+  getImageRoundnessStyle,
+  IconTokens,
+  ImageTokens,
+} from '../../../design-tokens';
+import {useAppTheme} from '../../../theme';
+import {BlurredBackground} from '../../containers/BlurredBackground';
+import {Icon} from '../../icons';
+import {Thumbnail} from '../../images';
+import {Layout} from '../../layout';
+import {BodyText, CaptionText} from '../../typography';
 
 export declare namespace TrackItem {
   export type Props = {
     track: Entities.Track;
     isPlaying?: boolean;
-    onPress?: (track: Entities.Track) => unknown
+    onPress?: (track: Entities.Track) => unknown;
   } & Pick<Layout.Props, 'style'>;
 }
 
-export function TrackItem({ track, isPlaying = false, onPress = noop, style }: TrackItem.Props) {
-  const artistNames = useMemo(() => (track.artists.reduce(
-    (names, artist) => (names ? `${names}, ${artist.name}` : artist.name),
-    '',
-  )), [track])
+export function TrackItem({
+  track,
+  isPlaying = false,
+  onPress = noop,
+  style,
+}: TrackItem.Props) {
+  const artistNames = useMemo(
+    () =>
+      track.artists.reduce(
+        (names, artist) => (names ? `${names}, ${artist.name}` : artist.name),
+        '',
+      ),
+    [track],
+  );
   return (
-    <TouchableNativeFeedback onPress={()=>{
-      onPress(track)
-    }}>
+    <TouchableNativeFeedback
+      onPress={() => {
+        onPress(track);
+      }}>
       <Layout
-        direction='row'
-        align='center center'
+        direction="row"
+        align="center center"
         paddingScale={2}
         style={style}>
         <Layout>
           {isPlaying && <ThumbnailMask />}
-          <Thumbnail source={{ uri: track.album.icon.url }} />
+          <Thumbnail source={{uri: track.album.thumbnail.url}} />
         </Layout>
-        <Layout flex={1} align='center start' paddingHorizontalScale={2}>
+        <Layout flex={1} align="center start" paddingHorizontalScale={2}>
           <BodyText isActive={isPlaying}>{track.name}</BodyText>
           <CaptionText>{artistNames}</CaptionText>
         </Layout>
       </Layout>
     </TouchableNativeFeedback>
-  )
+  );
 }
 
 function ThumbnailMask() {
-  const { colors } = useAppTheme()
+  const {colors} = useAppTheme();
   const styles: Layout.Props['style'][] = [
     getImageRoundnessStyle(ImageTokens.Roundness.Thumbnail),
-    {position: 'absolute'}
-  ]
+    {position: 'absolute'},
+  ];
   return (
-    <BlurredBackground
-      blurAmount={5}
-      blurType='light'
-      style={styles}>
+    <BlurredBackground blurAmount={5} blurType="light" style={styles}>
       <Layout
         flex={1}
-        align='center center'
-        style={{ zIndex: BlurredBackground.zIndex.NotBlurred }}>
+        align="center center"
+        style={{zIndex: BlurredBackground.zIndex.NotBlurred}}>
         <Icon
-          name='pause'
+          name="pause"
           size={IconTokens.Size.ControlsPrimary}
-          style={{ color: colors.text.button, opacity: 0.7 }}
+          style={{color: colors.text.button, opacity: 0.7}}
         />
       </Layout>
     </BlurredBackground>
-  )
+  );
 }
