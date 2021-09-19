da621c3b2c79856582d3975f2a4759aca61b58b7
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-19 21:01:30 +0500

    Added playingTrackId state

--- packages/spotify-native/app/components/features/playlist/Playlist.tsx
+++ packages/spotify-native/app/components/features/playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import {PlaylistFeature} from 'spotify-core';
 import {Cover} from '../../images';
@@ -13,15 +13,24 @@ export default function Playlist({playlistId}: Props) {
   const {fetchPlaylist, playlist} = PlaylistFeature.usePlaylist();
   useEffect(() => {
     fetchPlaylist(playlistId);
-  }, []);
+  }, [playlistId]);
   const tracks = playlist ? playlist.tracks : [];
+  const [playingTrackId, setPlayingTrackId] = useState(null);
   return (
     <Layout flex={1}>
       <Cover source={{uri: playlist?.backgroundImage.url}} />
       <FlatList
         data={tracks}
         renderItem={({item}) => {
-          return <TrackItem track={item} />;
+          return (
+            <TrackItem
+              track={item}
+              isPlaying={item.id === playingTrackId}
+              onPress={track => {
+                setPlayingTrackId(track.id);
+              }}
+            />
+          );
         }}
         alwaysBounceVertical
         keyExtractor={item => item.id}
