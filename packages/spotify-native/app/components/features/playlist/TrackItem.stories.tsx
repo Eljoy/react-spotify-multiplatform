40188de69f05bf3ba694af78b6797b882c519c8b
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-19 14:55:05 +0500

    Updated TrackItem.stories

--- packages/spotify-native/app/components/features/playlist/TrackItem.stories.tsx
+++ packages/spotify-native/app/components/features/playlist/TrackItem.stories.tsx
@@ -1,18 +1,30 @@
 import {storiesOf} from '@storybook/react-native';
-import React from 'react';
+import range from 'lodash.range';
+import React, {useState} from 'react';
+import {ScrollView} from 'react-native';
 import {Faker} from 'spotify-core';
 import {AppBackground} from '../../containers';
 import {TrackItem} from './TrackItem';
 
 storiesOf('Playlist', module).add('TrackItem', () => <TrackItemStory />);
 
+const trackItems = range(0, 30).map(Faker.Entities.generateTrack);
+
 function TrackItemStory() {
+  const [playingId, setPlayingId] = useState(null);
+  const trackItemNodes = trackItems.map(track => (
+    <TrackItem
+      isPlaying={track.id === playingId}
+      track={track}
+      onPress={() => {
+        setPlayingId(track.id);
+      }}
+    />
+  ));
+
   return (
     <AppBackground>
-      <TrackItem track={Faker.Entities.generateTrack()} />
-      <TrackItem track={Faker.Entities.generateTrack()} />
-      <TrackItem track={Faker.Entities.generateTrack()} />
-      <TrackItem track={Faker.Entities.generateTrack()} />
+      <ScrollView>{trackItemNodes}</ScrollView>
     </AppBackground>
   );
 }
