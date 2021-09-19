36e5e3fc2157d8586e6227a0e179bcf29f3112b4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-19 20:53:22 +0500

    Wrapped with AppBackground

--- packages/spotify-native/app/navigation/screens/PlaylistScreen.tsx
+++ packages/spotify-native/app/navigation/screens/PlaylistScreen.tsx
@@ -2,15 +2,16 @@ import {useNavigation, useRoute} from '@react-navigation/native';
 import React from 'react';
 import {Button} from 'react-native';
 import {Playlist} from '../../components';
+import {AppBackground} from '../../components/containers';
 
 export default function PlaylistScreen() {
   const route = useRoute();
   const {playlistId} = route.params as {playlistId: string};
   const {goBack} = useNavigation();
   return (
-    <>
+    <AppBackground>
       <Button title={'Go back'} onPress={goBack} />
       <Playlist playlistId={playlistId} />
-    </>
+    </AppBackground>
   );
 }
