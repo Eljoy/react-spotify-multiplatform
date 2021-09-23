30353348e318b2f6cb5459fe99064440a7ee209b
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-23 17:30:48 +0500

    Basic grow animation

--- packages/spotify-native/app/components/features/featured-playlists/PlaylistItem.tsx
+++ packages/spotify-native/app/components/features/featured-playlists/PlaylistItem.tsx
@@ -1,19 +1,26 @@
 import * as faker from 'faker';
-import React, {useState} from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import {
+  Dimensions,
   Image,
   ImageStyle,
   NativeModules,
   Platform,
   ScrollView,
+  StyleSheet,
+  Text,
   TouchableWithoutFeedback,
+  ViewStyle,
 } from 'react-native';
-import Animated, {useAnimatedRef} from 'react-native-reanimated';
+import Animated, {
+  useAnimatedRef,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 import {AppBackground} from '../../containers';
 import Layout from '../../layout/Layout';
-import {SubTitleText} from '../../typography';
 const {StatusBarManager} = NativeModules;
-
 const images = [
   'https://m.media-amazon.com/images/I/61d8t0gNa+L._SS500_.jpg',
   'https://www.udiscovermusic.com/wp-content/uploads/2015/10/Janelle-Mona%CC%81e-Dirty-Computer-.jpg',
@@ -21,9 +28,11 @@ const images = [
   'https://i.pinimg.com/originals/44/86/16/4486167e8fec4d4438a12707da02537f.jpg',
 ];
 
+const {width: wWidth, height: wHeight} = Dimensions.get('window');
+
 type Position = {
-  x: number;
-  y: number;
+  left: number;
+  top: number;
   width: number;
   height: number;
 };
@@ -34,12 +43,10 @@ const measure = async ref =>
   new Promise(resolve =>
     ref.measureInWindow((x, y, width, height, pageX, pageY) =>
       resolve({
-        x,
-        y: y - yOffset,
+        left: x,
+        top: y - yOffset,
         width,
         height,
-        pageX,
-        pageY,
       }),
     ),
   );
@@ -94,26 +101,64 @@ export function PlaylistItem({
 }
 
 function Modal({uri, position}: {uri: string; position: Position}) {
-  const imageStyle: ImageStyle[] = [
-    {
-      borderRadius: 8,
-      width: position.width,
-      height: position.height,
-    },
-  ];
+  const height = useSharedValue(position.height);
+  const width = useSharedValue(position.width);
+  const left = useSharedValue(position.left);
+  const top = useSharedValue(position.top);
+  const style = useAnimatedStyle<ViewStyle>(() => ({
+    height: height.value,
+    width: width.value,
+    transform: [{translateX: left.value}, {translateY: top.value}],
+    position: 'absolute',
+    zIndex: 100,
+  }));
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      width.value = withSpring(wWidth);
+      height.value = withSpring(wHeight);
+      left.value = withSpring(0);
+      top.value = withSpring(0);
+    }, 100);
+  }, []);
+
   return (
-    <Animated.View
-      style={{
-        position: 'absolute',
-        width: position.width,
-        left: position.x,
-        top: position.y,
-        zIndex: 100,
-      }}>
-      <AppBackground>
-        <Animated.Image source={{uri}} style={imageStyle} />
-        <SubTitleText>{faker.lorem.lines(10)}</SubTitleText>
-      </AppBackground>
+    <Animated.View style={style}>
+      <Test app={{source: {uri}}} borderRadius={8} />
     </Animated.View>
   );
 }
+
+function Test({app: {source}, borderRadius}) {
+  return (
+    <>
+      <Animated.Image style={[styles.image, {borderRadius: 8}]} {...{source}} />
+      <Layout style={[styles.content]}>
+        <Text style={styles.title}>{faker.lorem.text()}</Text>
+        <Text style={styles.subtitle}>{faker.lorem.lines(40)}</Text>
+      </Layout>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  content: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 100,
+    top: 300,
+  },
+  title: {
+    color: 'white',
+    fontSize: 34,
+    lineHeight: 41,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
