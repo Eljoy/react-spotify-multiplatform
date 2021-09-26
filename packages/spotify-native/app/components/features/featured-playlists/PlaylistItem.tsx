e3495be0a52fe8ab0b5149d30117eddf3103063d
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-09-26 19:25:37 +0500

    Incremental refactoring

--- packages/spotify-native/app/components/features/featured-playlists/PlaylistItem.tsx
+++ packages/spotify-native/app/components/features/featured-playlists/PlaylistItem.tsx
@@ -12,7 +12,12 @@ import {
   TouchableWithoutFeedback,
   ViewStyle,
 } from 'react-native';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
 import Animated, {
+  useAnimatedGestureHandler,
   useAnimatedRef,
   useAnimatedStyle,
   useSharedValue,
@@ -64,12 +69,21 @@ export function PlaylistItemContainer() {
   ));
   return (
     <AppBackground>
-      <ScrollView>
-        <Layout align="center center" paddingScale={0}>
-          {playlistItemNodes}
-        </Layout>
-      </ScrollView>
-      {modalParams && <Modal {...modalParams} />}
+      <GestureHandlerRootView>
+        <ScrollView>
+          <Layout align="center center" paddingScale={0}>
+            {playlistItemNodes}
+          </Layout>
+        </ScrollView>
+        {modalParams && (
+          <Modal
+            {...modalParams}
+            onClose={() => {
+              setModalParams(null);
+            }}
+          />
+        )}
+      </GestureHandlerRootView>
     </AppBackground>
   );
 }
@@ -84,7 +98,9 @@ export function PlaylistItem({
   onPress: (id: number, position: Position) => void;
 }) {
   const containerRef = useAnimatedRef();
-  const imageStyle: ImageStyle[] = [{width: 300, height: 300, borderRadius: 8}];
+  const imageStyle: ImageStyle[] = [
+    {width: 300, height: 300, borderRadius: 8, resizeMode: 'cover'},
+  ];
   const startTransition = async () => {
     const position = (await measure(containerRef.current)) as Position;
     onPress(id, position);
@@ -93,18 +109,34 @@ export function PlaylistItem({
     <Layout marginScale={3}>
       <TouchableWithoutFeedback onPress={startTransition}>
         <Animated.View ref={containerRef as any}>
-          <Image source={{uri}} style={imageStyle} />
+          <Image source={{uri}} style={imageStyle} resizeMode="cover" />
         </Animated.View>
       </TouchableWithoutFeedback>
     </Layout>
   );
 }
 
-function Modal({uri, position}: {uri: string; position: Position}) {
+function workletClamp(num: number, lower: number, upper: number) {
+  'worklet';
+  if (lower > upper) upper = [lower, (lower = upper)][0];
+  if (num <= lower) return lower;
+  if (num >= upper) return upper;
+  return num;
+}
+
+function Modal({
+  uri,
+  position,
+}: {
+  uri: string;
+  position: Position;
+  onClose: () => void;
+}) {
   const height = useSharedValue(position.height);
   const width = useSharedValue(position.width);
   const left = useSharedValue(position.left);
   const top = useSharedValue(position.top);
+  const borderRadius = useSharedValue(8);
   const style = useAnimatedStyle<ViewStyle>(() => ({
     height: height.value,
     width: width.value,
@@ -112,39 +144,154 @@ function Modal({uri, position}: {uri: string; position: Position}) {
     position: 'absolute',
     zIndex: 100,
   }));
+  const imageStyle = useAnimatedStyle<ImageStyle>(() => ({
+    width: width.value,
+    height: width.value,
+    borderRadius: borderRadius.value,
+  }));
+  const config: Animated.WithSpringConfig = {
+    stiffness: 150,
+  };
+
+  const getModalSharedTransitionParams = withModalSharedTransition(
+    {
+      leftValue: position.left,
+      borderRadiusValue: 8,
+      topValue: position.top,
+      widthValue: position.width,
+      heightValue: position.height,
+    },
+    {
+      leftValue: 0,
+      topValue: 0,
+      heightValue: wWidth,
+      widthValue: wWidth,
+      borderRadiusValue: 0,
+    },
+  );
+
   useLayoutEffect(() => {
     setTimeout(() => {
-      width.value = withSpring(wWidth);
-      height.value = withSpring(wHeight);
-      left.value = withSpring(0);
-      top.value = withSpring(0);
+      setModalAnimStyle(getModalSharedTransitionParams('opened'));
     }, 100);
   }, []);
 
+  function setModalAnimStyle({
+    leftValue,
+    topValue,
+    widthValue,
+    heightValue,
+    borderRadiusValue,
+  }: ModalTransitionParam) {
+    'worklet';
+    left.value = withSpring(leftValue, config);
+    top.value = withSpring(topValue, config);
+    width.value = withSpring(widthValue, config);
+    height.value = withSpring(heightValue, config);
+    borderRadius.value = withSpring(borderRadiusValue, config);
+  }
+
+  const eventTranslationThreshold = 20;
+  const onGesture = useAnimatedGestureHandler({
+    onActive: (event, context) => {
+      console.log('event.translationY ', event.translationY);
+      if (event.translationY >= eventTranslationThreshold) {
+        const params = getModalSharedTransitionParams('closed');
+        setModalAnimStyle(params);
+      } else {
+        const params = getModalSharedTransitionParams(
+          'active',
+          event.translationY,
+        );
+        setModalAnimStyle(params);
+      }
+    },
+    onEnd: _ => {
+      // top.value = withSpring(0);
+      // left.value = withSpring(0);
+    },
+  });
+
   return (
     <Animated.View style={style}>
-      <Test app={{source: {uri}}} borderRadius={8} />
+      <AppBackground flex={1}>
+        <PanGestureHandler onGestureEvent={onGesture}>
+          <Animated.Image style={[styles.image, imageStyle]} source={{uri}} />
+        </PanGestureHandler>
+        <Layout style={[styles.content]}>
+          <Text style={styles.title}>{faker.lorem.text()}</Text>
+          <Text style={styles.subtitle}>{faker.lorem.lines(40)}</Text>
+        </Layout>
+      </AppBackground>
     </Animated.View>
   );
 }
 
-function Test({app: {source}, borderRadius}) {
-  return (
-    <>
-      <Animated.Image style={[styles.image, {borderRadius: 8}]} {...{source}} />
-      <Layout style={[styles.content]}>
-        <Text style={styles.title}>{faker.lorem.text()}</Text>
-        <Text style={styles.subtitle}>{faker.lorem.lines(40)}</Text>
-      </Layout>
-    </>
-  );
+type ModalTransitionParam = {
+  leftValue: number;
+  topValue: number;
+  borderRadiusValue: number;
+  widthValue: number;
+  heightValue: number;
+};
+
+function withModalSharedTransition(
+  notStartedParams: ModalTransitionParam,
+  completeParams: ModalTransitionParam,
+) {
+  'worklet';
+  return function (
+    state: 'closed' | 'active' | 'opened',
+    translationY?: number,
+  ): ModalTransitionParam {
+    'worklet';
+    switch (state) {
+      case 'closed':
+        return notStartedParams;
+      case 'opened':
+        return completeParams;
+      case 'active':
+        const multiply = workletClamp(translationY / 100, 0, 1);
+        const leftValue = workletClamp(
+          notStartedParams.leftValue * multiply,
+          notStartedParams.leftValue,
+          completeParams.leftValue,
+        );
+        const topValue = workletClamp(
+          notStartedParams.topValue * multiply,
+          notStartedParams.topValue,
+          completeParams.topValue,
+        );
+        const widthValue = workletClamp(
+          completeParams.widthValue * (1 - multiply),
+          notStartedParams.widthValue,
+          completeParams.widthValue,
+        );
+        const heightValue = workletClamp(
+          completeParams.heightValue * (1 - multiply),
+          notStartedParams.heightValue,
+          completeParams.heightValue,
+        );
+        const borderRadiusValue = workletClamp(
+          notStartedParams.borderRadiusValue -
+            notStartedParams.borderRadiusValue * (1 - multiply),
+          notStartedParams.borderRadiusValue,
+          completeParams.borderRadiusValue,
+        );
+        return {
+          leftValue,
+          topValue,
+          widthValue,
+          heightValue,
+          borderRadiusValue,
+        };
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   content: {
-    ...StyleSheet.absoluteFillObject,
     zIndex: 100,
-    top: 300,
   },
   title: {
     color: 'white',
@@ -156,9 +303,5 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
   },
-  image: {
-    ...StyleSheet.absoluteFillObject,
-    width: undefined,
-    height: undefined,
-  },
+  image: {},
 });
